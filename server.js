5ed1067aa52461ea7f686ca4a30d680f66a23848
zanedeso <zanemilo@yahoo.com>
2025-07-05 13:58:07 -0600

    add file system module and load vocabulary and recipes data

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 // server.js
 const express = require('express');
 const path = require('path');
+const fs = require('fs'); // Node.js file system module to read files
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+const vocabularyPath = path.join(__dirname, 'data', 'vocabulary.json');
+const vocabulary = JSON.parse(fs.readFileSync(vocabularyPath, 'utf8'));
+
+const recipesPath = path.join(__dirname, 'data', 'recipes.json');
+const recipes = JSON.parse(fs.readFileSync(recipesPath, 'utf8'));
 
 // Middleware: serve static files from "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
